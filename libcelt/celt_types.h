2fecbdf084f74394183d198e6283c94e731ed5a5
Jean-Marc Valin <jean-marc.valin@usherbrooke.ca>
2008-01-31 23:43:10 +1100

    Fixed the default int32 type which was wrong on amd64 (and added testcase). Also, added an Ogg encoder (doesn't work quite yet).

--- libcelt/celt_types.h
+++ libcelt/celt_types.h
@@ -158,8 +158,8 @@
    /* Give up, take a reasonable guess */
    typedef short celt_int16_t;
    typedef unsigned short celt_uint16_t;
-   typedef long celt_int32_t;
-   typedef unsigned long celt_uint32_t;
+   typedef int celt_int32_t;
+   typedef unsigned int celt_uint32_t;
    typedef long long celt_int64_t;
    typedef unsigned long long celt_uint64_t;
 
---
+++ tests/type-test.c
@@ -0,0 +1,24 @@
+#include "celt_types.h"
+#include <stdio.h>
+
+int main()
+{
+   celt_int16_t i = 1;
+   i <<= 14;
+   if (i>>14 != 1)
+   {
+      fprintf(stderr, "celt_int16_t isn't 16 bits\n");
+      return 1;
+   }
+   if (sizeof(celt_int16_t)*2 != sizeof(celt_int32_t))
+   {
+      fprintf(stderr, "16*2 != 32\n");
+      return 1;
+   }
+   if (sizeof(celt_int32_t)*2 != sizeof(celt_int64_t))
+   {
+      fprintf(stderr, "32*2 != 64\n");
+      return 1;
+   }
+   return 0;
+}
