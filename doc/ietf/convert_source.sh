55ac2d1db0ec913df826f98dc079a5f91dc3b3ee
Jean-Marc Valin <jean-marc.valin@octasic.com>
2009-01-08 17:37:22 -0500

    Added all the source files to the draft

---
+++ doc/ietf/convert_source.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir -p source
+
+for i in `ls ../../libcelt | grep '\.[ch]$'`
+do
+
+#cat ../../libcelt/$i | sed -e 's/\&/\&amp;/g' -e 's/</\&lt;/g' -e 's/^/<t>/' -e 's/$/<\/t>/' > source/$i
+
+echo "<section anchor=\"$i\" title=\"$i\">" > source/$i
+echo '<t>' >> source/$i
+echo '<figure><artwork><![CDATA[' >> source/$i
+cat ../../libcelt/$i >> source/$i
+echo ']]></artwork></figure>' >> source/$i
+echo '</t>' >> source/$i
+echo '</section>' >> source/$i
+
+done
