86a7b7871d73b4563fbe9bc6ae8d2f70fb3cd636
Jean-Marc Valin <jean-marc.valin@octasic.com>
2009-07-02 16:09:03 -0400

    ietf doc: energy decoding, build script, misc stuff

---
+++ doc/ietf/build_drafts.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+./convert_source.sh
+
+./ietf_source.sh
+
+#codec draft
+xml2rfc.tcl draft-valin-celt-codec.xml draft-valin-celt-codec.html
+
+xml2rfc.tcl draft-valin-celt-codec.xml draft-valin-celt-codec.txt
+
+#RTP draft
+xml2rfc.tcl draft-valin-celt-rtp-profile.xml draft-valin-celt-rtp-profile.html
+
+xml2rfc.tcl draft-valin-celt-rtp-profile.xml draft-valin-celt-rtp-profile.txt
