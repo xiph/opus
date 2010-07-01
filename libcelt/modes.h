961f723ab756ac0a760a1b858c110720765a9a2c
Jean-Marc Valin <jean-marc.valin@usherbrooke.ca>
2010-07-01 10:14:20 -0400

    Prototypes for the _ec variants of the calls

--- libcelt/modes.h
+++ libcelt/modes.h
@@ -39,6 +39,8 @@
 #include "arch.h"
 #include "mdct.h"
 #include "pitch.h"
+#include "entenc.h"
+#include "entdec.h"
 
 #define MAX_CONFIG_SIZES 5
 
@@ -109,5 +111,11 @@ struct CELTMode {
 };
 
 int check_mode(const CELTMode *mode);
+
+/* Prototypes for _ec versions of the encoder/decoder calls (not public) */
+int celt_encode_with_ec(CELTEncoder * restrict st, const celt_int16 * pcm, celt_int16 * optional_resynthesis, int frame_size, unsigned char *compressed, int nbCompressedBytes, ec_enc *enc);
+int celt_encode_with_ec_float(CELTEncoder * restrict st, const float * pcm, float * optional_resynthesis, int frame_size, unsigned char *compressed, int nbCompressedBytes, ec_enc *enc);
+int celt_decode_with_ec(CELTDecoder * restrict st, const unsigned char *data, int len, celt_int16 * restrict pcm, int frame_size, ec_dec *dec);
+int celt_decode_with_ec_float(CELTDecoder * restrict st, const unsigned char *data, int len, celt_sig * restrict pcm, int frame_size, ec_dec *dec);
 
 #endif
