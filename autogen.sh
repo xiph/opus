67224e3c720904c17050c366d4f746ceeffb9049
Jean-Marc Valin <jean-marc.valin@octasic.com>
2010-07-08 11:25:43 -0400

    autotools details

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 # Run this to set up the build system: configure, makefiles, etc.
 # (based on the version in enlightenment's cvs)
 
-package="celt"
+package="silk"
 
 olddir=`pwd`
 srcdir=`dirname $0`
