27fc0c429061e4545a84ef6ea4cb69f4243847c6
Jean-Marc Valin <jean-marc.valin@octasic.com>
2010-10-18 15:10:33 -0400

    s/const/static const/

--- libcelt/quant_bands.c
+++ libcelt/quant_bands.c
@@ -44,7 +44,7 @@
 
 #ifdef FIXED_POINT
 /* Mean energy in each band quantized in Q6 */
-const signed char eMeans[25] = {
+static const signed char eMeans[25] = {
       103,100, 92, 85, 81,
        77, 72, 70, 78, 75,
        73, 71, 78, 74, 69,
@@ -53,7 +53,7 @@ const signed char eMeans[25] = {
 };
 #else
 /* Mean energy in each band quantized in Q6 and converted back to float */
-const celt_word16 eMeans[25] = {
+static const celt_word16 eMeans[25] = {
       6.437500f, 6.250000f, 5.750000f, 5.312500f, 5.062500f,
       4.812500f, 4.500000f, 4.375000f, 4.875000f, 4.687500f,
       4.562500f, 4.437500f, 4.875000f, 4.625000f, 4.312500f,
