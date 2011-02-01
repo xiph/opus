xiph/opus

commit 9b34bd835ca4e6c7597873cbb4e1ff47f7b9f0c3

Change qb cap to prevent side-fold collapses.
Previously, in a stereo split with itheta==16384, but without
 enough bits left over to actually code a pulse, the target band
 would completely collapse, because the mid gain would be zero and
 we don't fold the side.
This changes the limit to ensure that we never set qn>1 unless we
 know we'll have enough bits for at least one pulse.
This should eliminate the last possible whole-band collapse.

libcelt/bands.c
@@ -594,7 +594,7 @@ void haar1(celt_norm *X, int N0, int stride)
       }
 }
 
-static int compute_qn(int N, int b, int offset, int stereo)
+static int compute_qn(int N, int b, int offset, int pulse_cap, int stereo)
 {
    static const celt_int16 exp2_table8[8] =
       {16384, 17866, 19483, 21247, 23170, 25267, 27554, 30048};
@@ -602,9 +602,12 @@ static int compute_qn(int N, int b, int offset, int stereo)
    int N2 = 2*N-1;
    if (stereo && N==2)
       N2--;
-   qb = IMIN((b>>1)-(1<<BITRES), (b+N2*offset)/N2);
+   /* The upper limit ensures that in a stereo split with itheta==16384, we'll
+       always have enough bits left over to code at least one pulse in the
+       side; otherwise it would collapse, since it doesn't get folded. */
+   qb = IMIN(b-pulse_cap-(4<<BITRES), (b+N2*offset)/N2);
 
-   qb = IMAX(0, IMIN(8<<BITRES, qb));
+   qb = IMIN(8<<BITRES, qb);
 
    if (qb<(1<<BITRES>>1)) {
       qn = 1;
@@ -748,13 +751,15 @@ static unsigned quant_band(int encode, const CELTMode *m, int i, celt_norm *X, c
       int itheta=0;
       int mbits, sbits, delta;
       int qalloc;
+      int pulse_cap;
       int offset;
       int orig_fill;
       celt_int32 tell;
 
       /* Decide on the resolution to give to the split parameter theta */
-      offset = ((m->logN[i]+(LM<<BITRES))>>1) - (stereo ? QTHETA_OFFSET_STEREO : QTHETA_OFFSET);
-      qn = compute_qn(N, b, offset, stereo);
+      pulse_cap = m->logN[i]+(LM<<BITRES);
+      offset = (pulse_cap>>1) - (stereo ? QTHETA_OFFSET_STEREO : QTHETA_OFFSET);
+      qn = compute_qn(N, b, offset, pulse_cap, stereo);
       if (stereo && i>=intensity)
          qn = 1;
       if (encode)

commit 411a84faeada5092e2e94d8116b8adba45808183

Add a seprate qtheta offset for two-phase stereo.
9b34bd83 caused serious regressions for 240-sample frame stereo,
 because the previous qb limit was _always_ hit for two-phase
 stereo.
Two-phase stereo really does operate with a different model (for
 example, the single bit allocated to the side should really
 probably be thought of as a sign bit for qtheta, but we don't
 count it as part of qtheta's allocation).
The old code was equivalent to a separate two-phase offset of 12,
 however Greg Maxwell's testing demonstrates that 16 performs
 best.

libcelt/bands.c
@@ -769,7 +769,7 @@ static unsigned quant_band(int encode, const CELTMode *m, int i, celt_norm *X, c
 
       /* Decide on the resolution to give to the split parameter theta */
       pulse_cap = m->logN[i]+(LM<<BITRES);
-      offset = (pulse_cap>>1) - (stereo ? QTHETA_OFFSET_STEREO : QTHETA_OFFSET);
+      offset = (pulse_cap>>1) - (stereo&&N==2 ? QTHETA_OFFSET_TWOPHASE : QTHETA_OFFSET);
       qn = compute_qn(N, b, offset, pulse_cap, stereo);
       if (stereo && i>=intensity)
          qn = 1;

libcelt/rate.c
@@ -212,7 +212,7 @@ void compute_pulse_cache(CELTMode *m, int LM)
                if (C==2)
                {
                   max_bits <<= 1;
-                  offset = (m->logN[j]+(i<<BITRES)>>1)-QTHETA_OFFSET_STEREO;
+                  offset = (m->logN[j]+(i<<BITRES)>>1)-(N==2?QTHETA_OFFSET_TWOPHASE:QTHETA_OFFSET);
                   ndof = 2*N-1-(N==2);
                   /* The average measured cost for theta with the step PDF is
                       0.95164 times qb, approximated here as 487/512. */

libcelt/rate.h
@@ -43,7 +43,7 @@
 #define BITRES 3
 #define FINE_OFFSET 21
 #define QTHETA_OFFSET 4
-#define QTHETA_OFFSET_STEREO 4
+#define QTHETA_OFFSET_TWOPHASE 16
 
 #define BITOVERFLOW 30000
 