39ec0f9f0cace43c85e8ea6ef62f38ea8091aff4
Jean-Marc Valin <jean-marc.valin@usherbrooke.ca>
2011-02-03 12:30:32 -0500

    Automated draft compilation

---
+++ doc/build_draft.sh
@@ -0,0 +1,20 @@
+
+
+rm -rf opus_source
+cat opus_sources.mk celt_sources.mk silk_sources.mk opus_headers.txt celt_headers.txt silk_headers.txt | sed -e 's/^.*=//' -e 's/\\//' > all_files.txt
+tar czvf tmp_draft.tar.gz `cat all_files.txt`
+
+mkdir opus_source
+cd opus_source
+tar xzvf ../tmp_draft.tar.gz
+cp ../Makefile.draft Makefile
+cp ../opus_sources.mk .
+cp ../celt_sources.mk .
+cp ../silk_sources.mk .
+
+cd ..
+tar czvf opus_source.tar.gz opus_source
+cat opus_source.tar.gz| base64 -w 66 | sed 's/^/###/' > doc/opus_source.base64
+
+cd doc
+xml2rfc draft-ietf-codec-opus.xml
