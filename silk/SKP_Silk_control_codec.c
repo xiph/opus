ecb2ff7d86e898879802cddbb4ec73c0d736693a
Jean-Marc Valin <jean-marc.valin@octasic.com>
2011-05-02 11:26:15 -0400

    Including config.h (fixes the fixed-point)

--- silk/SKP_Silk_control_codec.c
+++ silk/SKP_Silk_control_codec.c
@@ -25,6 +25,10 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***********************************************************************/
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "SKP_Silk_tuning_parameters.h"
 #ifdef FIXED_POINT
 #include "SKP_Silk_main_FIX.h"
