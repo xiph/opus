51a643e4dcf984b13c11c14825108b9b17db3ed3
Jean-Marc Valin <jmvalin@jmvalin.ca>
2011-12-07 17:39:40 -0500

    Transient/VBR tuning, give more bits to frames where pitch changes

--- celt/celt.h
+++ celt/celt.h
@@ -54,6 +54,7 @@ typedef struct {
    int valid;
    opus_val16 tonality;
    opus_val16 tonality_slope;
+   opus_val16 noisiness;
    opus_val16 activity;
    int boost_band[2];
    opus_val16 boost_amount[2];
