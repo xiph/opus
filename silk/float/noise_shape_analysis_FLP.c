f7ceaa609a0c0c00b50e04d20fa4ca521ce5e052
Gregory Maxwell <greg@xiph.org>
2012-04-24 01:45:01 -0400

    Minor comment formatting change

--- silk/float/noise_shape_analysis_FLP.c
+++ silk/float/noise_shape_analysis_FLP.c
@@ -32,11 +32,10 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "main_FLP.h"
 #include "tuning_parameters.h"
 
-/* Compute gain to make warped filter coefficients have a zero mean log frequency response on a     */
-/* non-warped frequency scale. (So that it can be implemented with a minimum-phase monic filter.)   */
-/* Note: A monic filter is one with the first coefficient equal to 1.0. In Silk
-   we omit the first coefficient in an array of coefficients, for monic filters.
-*/
+/* Compute gain to make warped filter coefficients have a zero mean log frequency response on a   */
+/* non-warped frequency scale. (So that it can be implemented with a minimum-phase monic filter.) */
+/* Note: A monic filter is one with the first coefficient equal to 1.0. In Silk we omit the first */
+/* coefficient in an array of coefficients, for monic filters.                                    */
 static inline silk_float warped_gain(
     const silk_float     *coefs,
     silk_float           lambda,
