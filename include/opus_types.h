329112075e046ad7a17776a177564b46c17e1db6
Jean-Marc Valin <jmvalin@jmvalin.ca>
2012-05-10 16:00:19 -0400

    Addressing the remaining comments from Robert Sparks

--- include/opus_types.h
+++ include/opus_types.h
@@ -29,7 +29,7 @@
    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-/* opus_types.h taken from libogg */
+/* opus_types.h based on ogg_types.h from libogg */
 
 /**
    @file opus_types.h
