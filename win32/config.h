d2fa2edfd8697dfd15ebf75a7c3ac9dfbc3376ee
Gian-Carlo Pascutto <gcp@sjeng.org>
2012-07-12 20:23:56 +0200

    Make compiler settings consistent. Actually produce the libary, remove the opus_demo for now.

--- win32/config.h
+++ win32/config.h
@@ -18,4 +18,6 @@
 #pragma warning(disable : 4996)/* This function or variable may be unsafe. Consider using fopen_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details. */
 #endif
 
+#define OPUS_VERSION "0.9.11 win32"
+
 #endif CONFIG_H
