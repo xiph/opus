ed627f040d8c8e7ec76f7e6b7fd70b24696a86d1
Jean-Marc Valin <jmvalin@jmvalin.ca>
2012-10-19 19:57:19 -0400

    Attempt to have at least two bands that are coded
    This fixes a SWB hybrid issue where band 18 isn't folded and cannot even
    be folded because it's wider than band 17. This was causing noise to
    be injected instead.

--- celt/rate.c
+++ celt/rate.c
@@ -353,7 +353,7 @@ static inline int interp_bits2pulses(const CELTMode *m, int start, int end, int 
 #ifdef FUZZING
             if ((rand()&0x1) == 0)
 #else
-            if (band_bits > ((j<prev?7:9)*band_width<<LM<<BITRES)>>4)
+            if (codedBands<=start+2 || band_bits > ((j<prev?7:9)*band_width<<LM<<BITRES)>>4)
 #endif
             {
                ec_enc_bit_logp(ec, 1, 1);
