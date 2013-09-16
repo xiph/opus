f2446c25c6519bae190152f7a579310b83dc43fd
Ralph Giles <giles@mozilla.com>
2013-09-16 14:40:04 -0700

    Remove trailing whitespace from the license headers.

--- silk/tables_other.c
+++ silk/tables_other.c
@@ -8,7 +8,7 @@ this list of conditions and the following disclaimer.
 - Redistributions in binary form must reproduce the above copyright
 notice, this list of conditions and the following disclaimer in the
 documentation and/or other materials provided with the distribution.
-- Neither the name of Internet Society, IETF or IETF Trust, nor the 
+- Neither the name of Internet Society, IETF or IETF Trust, nor the
 names of specific contributors, may be used to endorse or promote
 products derived from this software without specific prior written
 permission.
