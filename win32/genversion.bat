a1740d9ad176ed3716b5c35cba6c47da733b789c
Ron <ron@debian.org>
2013-11-28 04:18:45 +1030

    Drop the use of --always to git-describe
    This is a partial revert of c435f06b308e52d7a5d2aad9eec98a1541832b47,
    since it didn't actually fix the original problem with shallow clones.
    It returns just a hash, not something we (currently) can extract as a
    version string in the form things are expecting.

--- win32/genversion.bat
+++ win32/genversion.bat
@@ -2,7 +2,7 @@
 
 setlocal enableextensions enabledelayedexpansion
 
-for /f %%v in ('git --git-dir="%~dp0..\.git" describe --tags --match "v*" --always') do set version=%%v
+for /f %%v in ('git --git-dir="%~dp0..\.git" describe --tags --match "v*"') do set version=%%v
 
 if not "%version%"=="" goto :gotversion
 
