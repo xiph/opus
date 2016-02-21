e7a53f74d52d78b9bc9c2b7f2c2efc1a1f6fafda
Koen Vos <koenvos@users.noreply.github.com>
2016-02-21 16:21:03 +0800

    Clean up: replace tabs by spaces

--- silk/float/k2a_FLP.c
+++ silk/float/k2a_FLP.c
@@ -42,12 +42,12 @@ void silk_k2a_FLP(
     silk_float rck, tmp1, tmp2;
 
     for( k = 0; k < order; k++ ) {
-		rck = rc[ k ];
+        rck = rc[ k ];
         for( n = 0; n < (k + 1) >> 1; n++ ) {
-			tmp1 = A[ n ];
-			tmp2 = A[ k - n - 1 ];
+            tmp1 = A[ n ];
+            tmp2 = A[ k - n - 1 ];
             A[ n ]         = tmp1 + tmp2 * rck;
-			A[ k - n - 1 ] = tmp2 + tmp1 * rck;
+            A[ k - n - 1 ] = tmp2 + tmp1 * rck;
         }
         A[ k ] = -rck;
     }
