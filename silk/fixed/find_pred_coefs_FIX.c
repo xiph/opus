8f6098383ea7060838e168952499183c4786eb42
Koen Vos <koenvos@users.noreply.github.com>
2016-06-01 13:14:32 +0800

    more conservative scaling of LTP corrs; remove assert that checks for negative residual energy; discard CB entries leading to such negative energies

--- silk/fixed/find_pred_coefs_FIX.c
+++ silk/fixed/find_pred_coefs_FIX.c
@@ -86,7 +86,7 @@ void silk_find_pred_coefs_FIX(
         ALLOC( XXLTP_Q17, psEnc->sCmn.nb_subfr * LTP_ORDER * LTP_ORDER, opus_int32 );
 
         /* LTP analysis */
-        silk_find_LTP_FIX( XXLTP_Q17, xXLTP_Q17, res_pitch, 
+        silk_find_LTP_FIX( XXLTP_Q17, xXLTP_Q17, res_pitch,
             psEncCtrl->pitchL, psEnc->sCmn.subfr_length, psEnc->sCmn.nb_subfr, psEnc->sCmn.arch );
 
         /* Quantize LTP gain parameters */
