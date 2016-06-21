7ca418797c4d4d132d098eb2ebbb9913cc36a375
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-06-21 12:38:25 -0400

    Moving FEC decision to the Opus encoder level (from SILK level)

--- silk/control.h
+++ silk/control.h
@@ -77,6 +77,9 @@ typedef struct {
     /* I:   Flag to enable in-band Forward Error Correction (FEC); 0/1                      */
     opus_int useInBandFEC;
 
+    /* I:   Flag to actually code in-band Forward Error Correction (FEC) in the current packet; 0/1 */
+    opus_int LBRR_coded;
+
     /* I:   Flag to enable discontinuous transmission (DTX); 0/1                            */
     opus_int useDTX;
 
