4b1f45e407ad9923e64f2f1a5689290575899f9c
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-07-06 22:25:59 -0400

    make len==0 return OPUS_INVALID_PACKET rather than OPUS_BAD_ARG for opus_packet_parse_impl()
    len<0 still returns OPUS_BAD_ARG

--- src/opus.c
+++ src/opus.c
@@ -201,8 +201,10 @@ int opus_packet_parse_impl(const unsigned char *data, opus_int32 len,
    opus_int32 pad = 0;
    const unsigned char *data0 = data;
 
-   if (size==NULL || len<1)
+   if (size==NULL || len<0)
       return OPUS_BAD_ARG;
+   if (len==0)
+      return OPUS_INVALID_PACKET;
 
    framesize = opus_packet_get_samples_per_frame(data, 48000);
 
