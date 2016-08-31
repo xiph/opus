737db27eee0de08141e1d9000f3bd4e59df90d47
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-08-31 02:15:55 -0400

    Only enable new hybrid folding behaviour with ENABLE_UPDATE_DRAFT

--- celt/bands.c
+++ celt/bands.c
@@ -1477,6 +1477,7 @@ void quant_all_bands(int encode, const CELTMode *m, int start, int end,
          b = 0;
       }
 
+#ifdef ENABLE_UPDATE_DRAFT
       if (resynth && (M*eBands[i]-N >= M*eBands[start] || i==start+1) && (update_lowband || lowband_offset==0))
             lowband_offset = i;
       if (i == start+1)
@@ -1490,6 +1491,11 @@ void quant_all_bands(int encode, const CELTMode *m, int start, int end,
          if (C==2)
             OPUS_COPY(&norm2[n1], &norm2[2*n1 - n2], n2-n1);
       }
+#else
+      if (resynth && M*eBands[i]-N >= M*eBands[start] && (update_lowband || lowband_offset==0))
+            lowband_offset = i;
+#endif
+
       tf_change = tf_res[i];
       ctx.tf_change = tf_change;
       if (i>=m->effEBands)
@@ -1514,7 +1520,11 @@ void quant_all_bands(int encode, const CELTMode *m, int start, int end,
          fold_start = lowband_offset;
          while(M*eBands[--fold_start] > effective_lowband+norm_offset);
          fold_end = lowband_offset-1;
+#ifdef ENABLE_UPDATE_DRAFT
          while(++fold_end < i && M*eBands[fold_end] < effective_lowband+norm_offset+N);
+#else
+         while(M*eBands[++fold_end] < effective_lowband+norm_offset+N);
+#endif
          x_cm = y_cm = 0;
          fold_i = fold_start; do {
            x_cm |= collapse_masks[fold_i*C+0];
