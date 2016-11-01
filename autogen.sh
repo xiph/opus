d31f883c823b6ff09c84ab4e4263ae0868db9bba
Jean-Marc Valin <jmvalin@jmvalin.ca>
2016-11-01 19:00:34 -0400

    Add copyright notice to autogen.sh to make the Chromium people happy

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright (c) 2010-2015 Xiph.Org Foundation and contributors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the COPYING file.
+
 # Run this to set up the build system: configure, makefiles, etc.
 set -e
 
