3c9b4d615cac46c686b260efb03713415855277b
Jean-Marc Valin <jmvalin@jmvalin.ca>
2017-02-15 17:34:24 -0500

    Increasing GLOBAL_STACK_SIZE to 120000 to avoid failure
    Cannot prove it's the correct value, but it's better than the previous
    values, which sometimes segfaults. The increase was made necessary due
    to 120 ms frame size support.

--- celt/arch.h
+++ celt/arch.h
@@ -260,9 +260,9 @@ static OPUS_INLINE int celt_isnan(float x)
 
 #ifndef GLOBAL_STACK_SIZE
 #ifdef FIXED_POINT
-#define GLOBAL_STACK_SIZE 100000
+#define GLOBAL_STACK_SIZE 120000
 #else
-#define GLOBAL_STACK_SIZE 100000
+#define GLOBAL_STACK_SIZE 120000
 #endif
 #endif
 
