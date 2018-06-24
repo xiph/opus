61c6391c210b4f17a1415e545676e56c50aab4e4
Jean-Marc Valin <jmvalin@jmvalin.ca>
2018-06-24 02:41:36 -0400

    Importing DSP code from RNNoise

--- dnn/train_lpcnet.py
+++ dnn/train_lpcnet.py
@@ -4,13 +4,20 @@ import lpcnet
 import sys
 import numpy as np
 from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 from ulaw import ulaw2lin, lin2ulaw
 
-nb_epochs = 10
-batch_size = 32
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.44
+set_session(tf.Session(config=config))
+
+nb_epochs = 40
+batch_size = 64
 
 model = lpcnet.new_wavernn_model()
-model.compile(optimizer=Adam(0.001), loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
+model.compile(optimizer=Adam(0.0008), loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
 model.summary()
 
 pcmfile = sys.argv[1]
@@ -28,4 +35,8 @@ out_data = lin2ulaw(data[1:1+nb_frames*chunk_size]) + 128
 in_data = np.reshape(in_data, (nb_frames, chunk_size, 1))
 out_data = np.reshape(out_data, (nb_frames, chunk_size, 1))
 
-model.fit(in_data, out_data, batch_size=batch_size, epochs=nb_epochs, validation_split=0.2)
+checkpoint = ModelCheckpoint('wavernn1f_{epoch:02d}.h5')
+
+#model.load_weights('wavernn1c_01.h5')
+model.compile(optimizer=Adam(0.002, amsgrad=True, decay=1e-4), loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
+model.fit(in_data, out_data, batch_size=batch_size, epochs=30, validation_split=0.2, callbacks=[checkpoint])
