6c2f7e58fdd4d73b24747428230c20eda0890728
Jean-Marc Valin <jmvalin@jmvalin.ca>
2018-11-27 15:08:04 -0500

    compiling synthesis

--- dnn/compile.sh
+++ dnn/compile.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 gcc -DTRAINING=1 -Wall -W -O3 -g -I../include denoise.c kiss_fft.c pitch.c celt_lpc.c -o dump_data -lm
+gcc -o test_lpcnet -g -O2 -Wall -W -Wextra lpcnet.c nnet.c nnet_data.c -lm
