1956467d794ba7efe053e260ab60ccd516890b3c
Jean-Marc Valin <jmvalin@jmvalin.ca>
2018-11-30 01:55:23 -0500

    Add AVX2/FMA to gcc options

--- dnn/compile.sh
+++ dnn/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 gcc -DTRAINING=1 -Wall -W -O3 -g -I../include denoise.c kiss_fft.c pitch.c celt_lpc.c -o dump_data -lm
-gcc -o test_lpcnet -g -O2 -Wall -W -Wextra lpcnet.c nnet.c nnet_data.c -lm
+gcc -o test_lpcnet -mavx2 -mfma -g -O3 -Wall -W -Wextra lpcnet.c nnet.c nnet_data.c -lm
