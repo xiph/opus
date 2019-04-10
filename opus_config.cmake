056d8a9f51da93121cdc002abe9deada1dc869d1
Marcus Asteborg <maastebo@microsoft.com>
2019-04-10 16:34:01 -0700

    CMake changes - Make release build default, made CUSTOM_MODE an option with default off and added missing buildflags for Linux and security.

--- opus_config.cmake
+++ opus_config.cmake
@@ -3,26 +3,11 @@ include(opus_functions.cmake)
 configure_file(config.h.cmake.in config.h @ONLY)
 add_definitions(-DHAVE_CONFIG_H)
 
-option(FIXED_POINT "Use fixed-point code (for devices with less powerful FPU)"
-       OFF)
-option(USE_ALLOCA "Use alloca for stack arrays (on non-C99 compilers)" OFF)
-option(BUILD_PROGRAMS "Build programs" OFF)
-
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 set_property(GLOBAL PROPERTY C_STANDARD 99)
 
 if(MSVC)
   add_definitions(-D_CRT_SECURE_NO_WARNINGS)
-endif()
-
-# It is strongly recommended to uncomment one of these VAR_ARRAYS: Use C99
-# variable-length arrays for stack allocation USE_ALLOCA: Use alloca() for stack
-# allocation If none is defined, then the fallback is a non-threadsafe global
-# array
-if(USE_ALLOCA OR MSVC)
-  add_definitions(-DUSE_ALLOCA)
-else()
-  add_definitions(-DVAR_ARRAYS)
 endif()
 
 include(CheckLibraryExists)
