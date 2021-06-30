f0ce43389ad630f0594be81ce1716b2b72e8c883
Jean-Marc Valin <jmvalin@amazon.com>
2021-06-30 18:54:27 -0400

    Update test_lpcnet.py, remove old TF1 code

--- dnn/training_tf2/test_lpcnet.py
+++ dnn/training_tf2/test_lpcnet.py
@@ -28,19 +28,11 @@
 import lpcnet
 import sys
 import numpy as np
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
 from ulaw import ulaw2lin, lin2ulaw
-import keras.backend as K
 import h5py
 
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.2
-set_session(tf.Session(config=config))
 
-model, enc, dec = lpcnet.new_lpcnet_model(use_gpu=False)
+model, enc, dec = lpcnet.new_lpcnet_model()
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
 #model.summary()
@@ -63,7 +55,7 @@ periods = (.1 + 50*features[:,:,36:37]+100).astype('int16')
 
 
 
-model.load_weights('lpcnet20h_384_10_G16_80.h5')
+model.load_weights('lpcnet34bq17_384_01.h5')
 
 order = 16
 
