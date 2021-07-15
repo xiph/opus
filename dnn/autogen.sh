4c0e2248655c3042c4cd93b3dd663cc798e5aa22
Jean-Marc Valin <jmvalin@amazon.com>
2021-07-15 16:12:42 -0400

    Model update
    Weights are the same, but they are dumped to C differently.

--- dnn/autogen.sh
+++ dnn/autogen.sh
@@ -6,7 +6,7 @@ srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
 #SHA1 of the first commit compatible with the current model
-commit=0e434ab
+commit=c1e85f8
 
 if [ ! -f lpcnet_data-$commit.tar.gz ]; then
 	echo "Downloading latest model"
