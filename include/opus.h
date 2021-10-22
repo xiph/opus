99afa9c48e934711f5b07a917b68411353453338
Alexander Traud <pabstraud@compuserve.com>
2021-10-22 12:19:42 +0000

    Fix opus.h for doxygen when referencing alternative values
    Doxygen was not able to resolve the references because it looked
    for OPUS_APPLICATION_VOIP/@ref.
    
    Signed-off-by: Mark Harris <mark.hsj@gmail.com>

--- include/opus.h
+++ include/opus.h
@@ -198,7 +198,7 @@ OPUS_EXPORT OPUS_WARN_UNUSED_RESULT int opus_encoder_get_size(int channels);
  *                                     This must be one of 8000, 12000, 16000,
  *                                     24000, or 48000.
  * @param [in] channels <tt>int</tt>: Number of channels (1 or 2) in input signal
- * @param [in] application <tt>int</tt>: Coding mode (@ref OPUS_APPLICATION_VOIP/@ref OPUS_APPLICATION_AUDIO/@ref OPUS_APPLICATION_RESTRICTED_LOWDELAY)
+ * @param [in] application <tt>int</tt>: Coding mode (@ref OPUS_APPLICATION_VOIP|@ref OPUS_APPLICATION_AUDIO|@ref OPUS_APPLICATION_RESTRICTED_LOWDELAY)
  * @param [out] error <tt>int*</tt>: @ref opus_errorcodes
  * @note Regardless of the sampling rate and number channels selected, the Opus encoder
  * can switch to a lower audio bandwidth or number of channels if the bitrate
