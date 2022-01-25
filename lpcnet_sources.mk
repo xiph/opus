f49cba8881c3aa3ccd16313d98a46a147e71db78
Jean-Marc Valin <jmvalin@amazon.com>
2022-01-25 02:48:51 -0500

    WIP: Using LPCNet for PLC

---
+++ lpcnet_sources.mk
@@ -0,0 +1,11 @@
+LPCNET_SOURCES = \
+lpcnet/src/ceps_codebooks.c \
+lpcnet/src/common.c \
+lpcnet/src/freq.c \
+lpcnet/src/kiss99.c \
+lpcnet/src/lpcnet.c \
+lpcnet/src/lpcnet_dec.c \
+lpcnet/src/lpcnet_enc.c \
+lpcnet/src/lpcnet_plc.c \
+lpcnet/src/nnet.c \
+lpcnet/src/nnet_data.c
