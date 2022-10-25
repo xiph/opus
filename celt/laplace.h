a113011b9e939e0f232f4c45ec5d23239f2f1ec5
Jan Buethe <jbuethe@amazon.de>
2022-10-25 12:17:35 +0000

    added some prototypes to header file

--- celt/laplace.h
+++ celt/laplace.h
@@ -46,3 +46,7 @@ void ec_laplace_encode(ec_enc *enc, int *value, unsigned fs, int decay);
  @return Value decoded
  */
 int ec_laplace_decode(ec_dec *dec, unsigned fs, int decay);
+
+
+int ec_laplace_decode_p0(ec_dec *dec, opus_uint16 p0, opus_uint16 decay);
+void ec_laplace_encode_p0(ec_enc *enc, int value, opus_uint16 p0, opus_uint16 decay);
