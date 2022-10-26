a223122b8946c7929081de33d8dada66646b35aa
Jan Buethe <jbuethe@amazon.de>
2022-10-26 12:42:34 +0000

    clean-up

--- dnn/dred_rdovae_dec.c
+++ dnn/dred_rdovae_dec.c
@@ -32,11 +32,7 @@
 
 #include "dred_rdovae_dec.h"
 #include "dred_rdovae_constants.h"
-//#define DEBUG
 
-#ifdef DEBUG
-#include <stdio.h>
-#endif
 
 void dred_rdovae_dec_init_states(
     RDOVAEDec *h,            /* io: state buffer handle */
@@ -59,78 +55,40 @@ void dred_rdovae_decode_qframe(
     float buffer[DEC_DENSE1_OUT_SIZE + DEC_DENSE2_OUT_SIZE + DEC_DENSE3_OUT_SIZE + DEC_DENSE4_OUT_SIZE + DEC_DENSE5_OUT_SIZE + DEC_DENSE6_OUT_SIZE + DEC_DENSE7_OUT_SIZE + DEC_DENSE8_OUT_SIZE];
     int output_index = 0;
     int input_index = 0;
-#ifdef DEBUG
-    static FILE *fids[8] = {NULL};
-    int i;
-    char filename[256];
-
-    for (i=0; i < 8; i ++)
-    {
-        if (fids[i] == NULL)
-        {
-            sprintf(filename, "y%d.f32", i + 1);
-            fids[i] = fopen(filename, "wb");
-        }
-    }
-#endif
 
     /* run encoder stack and concatenate output in buffer*/
     _lpcnet_compute_dense(&dec_dense1, &buffer[output_index], input);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), DEC_DENSE1_OUT_SIZE, fids[0]);
-#endif
     input_index = output_index;
     output_index += DEC_DENSE1_OUT_SIZE;
 
     compute_gru2(&dec_dense2, dec_state->dense2_state, &buffer[input_index]);
     memcpy(&buffer[output_index], dec_state->dense2_state, DEC_DENSE2_OUT_SIZE * sizeof(float));
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), DEC_DENSE2_OUT_SIZE, fids[1]);
-#endif
     input_index = output_index;
     output_index += DEC_DENSE2_OUT_SIZE;
 
     _lpcnet_compute_dense(&dec_dense3, &buffer[output_index], &buffer[input_index]);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), DEC_DENSE3_OUT_SIZE, fids[2]);
-#endif
     input_index = output_index;
     output_index += DEC_DENSE3_OUT_SIZE;
 
     compute_gru2(&dec_dense4, dec_state->dense4_state, &buffer[input_index]);
     memcpy(&buffer[output_index], dec_state->dense4_state, DEC_DENSE4_OUT_SIZE * sizeof(float));
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), DEC_DENSE4_OUT_SIZE, fids[3]);
-#endif
     input_index = output_index;
     output_index += DEC_DENSE4_OUT_SIZE;
 
     _lpcnet_compute_dense(&dec_dense5, &buffer[output_index], &buffer[input_index]);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), DEC_DENSE5_OUT_SIZE, fids[4]);
-#endif
     input_index = output_index;
     output_index += DEC_DENSE5_OUT_SIZE;
 
     compute_gru2(&dec_dense6, dec_state->dense6_state, &buffer[input_index]);
     memcpy(&buffer[output_index], dec_state->dense6_state, DEC_DENSE6_OUT_SIZE * sizeof(float));
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), DEC_DENSE6_OUT_SIZE, fids[5]);
-#endif
     input_index = output_index;
     output_index += DEC_DENSE6_OUT_SIZE;
 
     _lpcnet_compute_dense(&dec_dense7, &buffer[output_index], &buffer[input_index]);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), DEC_DENSE7_OUT_SIZE, fids[6]);
-#endif
     input_index = output_index;
     output_index += DEC_DENSE7_OUT_SIZE;
 
     _lpcnet_compute_dense(&dec_dense8, &buffer[output_index], &buffer[input_index]);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), DEC_DENSE8_OUT_SIZE, fids[7]);
-#endif
     output_index += DEC_DENSE8_OUT_SIZE;
 
     _lpcnet_compute_dense(&dec_final, qframe, buffer);
--- dnn/dred_rdovae_enc.c
+++ dnn/dred_rdovae_enc.c
@@ -35,12 +35,6 @@
 #include "dred_rdovae_enc.h"
 
 
-//#define DEBUG
-
-#ifdef DEBUG
-#include <stdio.h>
-#endif
-
 void dred_rdovae_encode_dframe(
     RDOVAEEnc *enc_state,           /* io: encoder state */
     float *latents,                 /* o: latent vector */
@@ -51,90 +45,43 @@ void dred_rdovae_encode_dframe(
     float buffer[ENC_DENSE1_OUT_SIZE + ENC_DENSE2_OUT_SIZE + ENC_DENSE3_OUT_SIZE + ENC_DENSE4_OUT_SIZE + ENC_DENSE5_OUT_SIZE + ENC_DENSE6_OUT_SIZE + ENC_DENSE7_OUT_SIZE + ENC_DENSE8_OUT_SIZE + GDENSE1_OUT_SIZE];
     int output_index = 0;
     int input_index = 0;
-#ifdef DEBUG
-    static FILE *fids[8] = {NULL};
-    static FILE *fpre = NULL;
-    int i;
-    char filename[256];
-
-    for (i=0; i < 8; i ++)
-    {
-        if (fids[i] == NULL)
-        {
-            sprintf(filename, "x%d.f32", i + 1);
-            fids[i] = fopen(filename, "wb");
-        }
-    }
-    if (fpre == NULL)
-    {
-        fpre = fopen("x_pre.f32", "wb");
-    }
-#endif
-
 
     /* run encoder stack and concatenate output in buffer*/
     _lpcnet_compute_dense(&enc_dense1, &buffer[output_index], input);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), ENC_DENSE1_OUT_SIZE, fids[0]);
-#endif
     input_index = output_index;
     output_index += ENC_DENSE1_OUT_SIZE;
 
     compute_gru2(&enc_dense2, enc_state->dense2_state, &buffer[input_index]);
     memcpy(&buffer[output_index], enc_state->dense2_state, ENC_DENSE2_OUT_SIZE * sizeof(float));
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), ENC_DENSE2_OUT_SIZE, fids[1]);
-#endif
     input_index = output_index;
     output_index += ENC_DENSE2_OUT_SIZE;
 
     _lpcnet_compute_dense(&enc_dense3, &buffer[output_index], &buffer[input_index]);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), ENC_DENSE3_OUT_SIZE, fids[2]);
-#endif
     input_index = output_index;
     output_index += ENC_DENSE3_OUT_SIZE;
 
     compute_gru2(&enc_dense4, enc_state->dense4_state, &buffer[input_index]);
     memcpy(&buffer[output_index], enc_state->dense4_state, ENC_DENSE4_OUT_SIZE * sizeof(float));
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), ENC_DENSE4_OUT_SIZE, fids[3]);
-#endif
     input_index = output_index;
     output_index += ENC_DENSE4_OUT_SIZE;
 
     _lpcnet_compute_dense(&enc_dense5, &buffer[output_index], &buffer[input_index]);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), ENC_DENSE5_OUT_SIZE, fids[4]);
-#endif
     input_index = output_index;
     output_index += ENC_DENSE5_OUT_SIZE;
 
     compute_gru2(&enc_dense6, enc_state->dense6_state, &buffer[input_index]);
     memcpy(&buffer[output_index], enc_state->dense6_state, ENC_DENSE6_OUT_SIZE * sizeof(float));
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), ENC_DENSE6_OUT_SIZE, fids[5]);
-#endif
     input_index = output_index;
     output_index += ENC_DENSE6_OUT_SIZE;
 
     _lpcnet_compute_dense(&enc_dense7, &buffer[output_index], &buffer[input_index]);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), ENC_DENSE7_OUT_SIZE, fids[6]);
-#endif
     input_index = output_index;
     output_index += ENC_DENSE7_OUT_SIZE;
 
     _lpcnet_compute_dense(&enc_dense8, &buffer[output_index], &buffer[input_index]);
-#ifdef DEBUG
-    fwrite(&buffer[output_index], sizeof(buffer[0]), ENC_DENSE8_OUT_SIZE, fids[7]);
-#endif
     output_index += ENC_DENSE8_OUT_SIZE;
 
     /* compute latents from concatenated input buffer */
-#ifdef DEBUG
-    fwrite(buffer, sizeof(buffer[0]), bits_dense.nb_inputs, fpre);
-#endif
     compute_conv1d(&bits_dense, latents, enc_state->bits_dense_state, buffer);
 
 
