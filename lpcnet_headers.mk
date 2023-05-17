7771b013952847831628fcd7b8364f5f99cf9e7e
Jean-Marc Valin <jmvalin@amazon.com>
2023-05-17 15:33:46 -0400

    Add LPCNet headers to make dist

---
+++ lpcnet_headers.mk
@@ -0,0 +1,26 @@
+LPCNET_HEAD = \
+silk/dred_coding.h \
+silk/dred_config.h \
+silk/dred_decoder.h \
+silk/dred_encoder.h \
+lpcnet/include/dred_rdovae.h \
+lpcnet/include/lpcnet.h \
+lpcnet/src/burg.h \
+lpcnet/src/common.h \
+lpcnet/src/freq.h \
+lpcnet/src/kiss99.h \
+lpcnet/src/lpcnet_private.h \
+lpcnet/src/nnet_data.h \
+lpcnet/src/nnet.h \
+lpcnet/src/plc_data.h \
+lpcnet/src/tansig_table.h \
+lpcnet/src/vec.h \
+lpcnet/src/vec_avx.h \
+lpcnet/src/vec_neon.h \
+lpcnet/src/dred_rdovae_constants.h \
+lpcnet/src/dred_rdovae_enc.h \
+lpcnet/src/dred_rdovae_enc_data.h \
+lpcnet/src/dred_rdovae_dec.h \
+lpcnet/src/dred_rdovae_dec_data.h \
+lpcnet/src/dred_rdovae_stats_data.h \
+lpcnet/src/pitch.h
