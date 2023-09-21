4af5f57415ee4d6eca4a757afc4a503d54ff3abf
Jean-Marc Valin <jmvalin@amazon.com>
2023-09-21 13:46:08 -0400

    Model update

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ set -e
 srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
-dnn/download_model.sh 574c766
+dnn/download_model.sh 9615893
 
 echo "Updating build configuration files, please wait...."
 
