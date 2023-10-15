6ea9312a93baa663a4695ce89d7d0691a3abb2ce
Jean-Marc Valin <jmvalin@amazon.com>
2023-10-15 03:39:40 -0400

    Only compile PLC/DRED conditionally

--- lpcnet_sources.mk
+++ lpcnet_sources.mk
@@ -1,4 +1,4 @@
-LPCNET_SOURCES = \
+DEEP_PLC_SOURCES = \
 dnn/burg.c \
 dnn/freq.c \
 dnn/fargan.c \
@@ -9,14 +9,15 @@ dnn/lpcnet_tables.c \
 dnn/nnet.c \
 dnn/plc_data.c \
 dnn/parse_lpcnet_weights.c \
+dnn/pitchdnn.c \
+dnn/pitchdnn_data.c
+DRED_SOURCES = \
 dnn/dred_rdovae.c \
 dnn/dred_rdovae_enc.c \
 dnn/dred_rdovae_enc_data.c \
 dnn/dred_rdovae_dec.c \
 dnn/dred_rdovae_dec_data.c \
 dnn/dred_rdovae_stats_data.c \
-dnn/pitchdnn.c \
-dnn/pitchdnn_data.c \
 silk/dred_encoder.c \
 silk/dred_coding.c \
 silk/dred_decoder.c
