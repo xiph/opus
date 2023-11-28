72cc88dfddce319aeac075bd28eff791cd2b14d8
Jean-Marc Valin <jmvalin@amazon.com>
2023-11-28 04:11:53 -0500

    FARGAN model update
    Finished adversarial training on 800k model. Also, move weights to a new
    location.

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ set -e
 srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
-dnn/download_model.sh 7f7b2a1
+dnn/download_model.sh df63771
 
 echo "Updating build configuration files, please wait...."
 
--- dnn/download_model.sh
+++ dnn/download_model.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-model=lpcnet_data-$1.tar.gz
+model=opus_data-$1.tar.gz
 
 if [ ! -f $model ]; then
         echo "Downloading latest model"
-        wget https://media.xiph.org/lpcnet/data/$model
+        wget https://media.xiph.org/opus/models/$model
 fi
 tar xvomf $model
