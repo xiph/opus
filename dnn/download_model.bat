5578824258c4ceea28b010a60de4a535bb028d87
Jean-Marc Valin <jmvalin@jmvalin.ca>
2023-11-28 23:39:48 -0500

    Fix model download path for windows

--- dnn/download_model.bat
+++ dnn/download_model.bat
@@ -1,9 +1,9 @@
 @echo off
-set model=lpcnet_data-%1.tar.gz
+set model=opus_data-%1.tar.gz
 
 if not exist %model% (
     echo Downloading latest model
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://media.xiph.org/lpcnet/data/%model%', '%model%')"
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://media.xiph.org/opus/models/%model%', '%model%')"
 )
 
 tar -xvzf %model%
