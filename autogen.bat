f126bfc531b9bf61fb92af70ee0a138cd7d0f99a
Michael Klingbeil <klingm@amazon.com>
2023-11-29 13:38:45 -0500

    fix autogen.bat model download

--- autogen.bat
+++ autogen.bat
@@ -4,11 +4,9 @@ REM Run this to set up the build system: configure, makefiles, etc.
 setlocal enabledelayedexpansion
 
 REM Parse the real autogen.sh script for version
-for /F "tokens=4 delims= " %%A in ('findstr "dnn\download_model.sh" autogen.sh') do (
+for /F "tokens=2 delims= " %%A in ('findstr "dnn/download_model.sh" autogen.sh') do (
     set "model=%%A"
 )
-REM Remove trailing ")" character from the model variable
-set "model=%model:~0,-1%"
 
 call dnn\download_model.bat %model%
 
