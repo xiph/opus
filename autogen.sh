3499d0aac76d20ba14918cafb8020278154bf2e6
Jan Buethe <jbuethe@amazon.de>
2024-01-22 15:23:09 +0100

    switched to smaller NoLACE model

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ set -e
 srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
-dnn/download_model.sh e699263
+dnn/download_model.sh ec04a94
 
 echo "Updating build configuration files, please wait...."
 
