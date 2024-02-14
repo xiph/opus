dd0e2dc3391ae20a1c05c91a5ee5d4179e27062f
Jan Buethe <jbuethe@amazon.de>
2024-02-14 10:37:15 +0100

    updated model

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ set -e
 srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
-dnn/download_model.sh ec04a94
+dnn/download_model.sh "4b9d9b0"
 
 echo "Updating build configuration files, please wait...."
 
