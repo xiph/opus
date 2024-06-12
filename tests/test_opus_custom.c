15521982481f992974732f99559d8733c80dab80
Jean-Marc Valin <jeanmarcv@google.com>
2024-06-12 12:57:48 -0400

    Adds 24-bit Opus encoder/decoder API

--- tests/test_opus_custom.c
+++ tests/test_opus_custom.c
@@ -217,16 +217,31 @@ int test_encode(TestCustomParams params) {
             }
          }
          else {
-            opus_int16* input = (opus_int16*)inbuf;
-            len = opus_encode(enc,
-                              &input[samp_count*num_channels],
-                              frame_size,
-                              packet,
-                              MAX_PACKET);
-            if (len <= 0) {
-                fprintf(stderr, "opus_encode() failed: %s\n", opus_strerror(len));
-                ret = -1;
-                break;
+            if (params.encoder_bit_depth == 24) {
+               opus_int32* input = (opus_int32*)inbuf;
+               len = opus_encode24(enc,
+                                   &input[samp_count*num_channels],
+                                   frame_size,
+                                   packet,
+                                   MAX_PACKET);
+               if (len <= 0) {
+                  fprintf(stderr, "opus_encode24() failed: %s\n", opus_strerror(len));
+                  ret = -1;
+                  break;
+               }
+            }
+            else {
+               opus_int16* input = (opus_int16*)inbuf;
+               len = opus_encode(enc,
+                                 &input[samp_count*num_channels],
+                                 frame_size,
+                                 packet,
+                                 MAX_PACKET);
+               if (len <= 0) {
+                   fprintf(stderr, "opus_encode() failed: %s\n", opus_strerror(len));
+                   ret = -1;
+                   break;
+               }
             }
          }
       }
@@ -294,17 +309,34 @@ int test_encode(TestCustomParams params) {
             }
          }
          else {
-            opus_int16* output = (opus_int16*)outbuf;
-            samples_decoded = opus_decode(dec,
-                                          packet,
-                                          len,
-                                          &output[samp_count*num_channels],
-                                          frame_size,
-                                          0);
-            if (samples_decoded != frame_size) {
-                fprintf(stderr, "opus_decode() returned %d\n", samples_decoded);
-                ret = -1;
-                break;
+            if (params.decoder_bit_depth == 24) {
+               opus_int32* output = (opus_int32*)outbuf;
+               samples_decoded = opus_decode24(dec,
+                                               packet,
+                                               len,
+                                               &output[samp_count*num_channels],
+                                               frame_size,
+                                               0);
+
+               if (samples_decoded != frame_size) {
+                  fprintf(stderr, "opus_decode24() returned %d\n", samples_decoded);
+                  ret = -1;
+                  break;
+               }
+            }
+            else {
+               opus_int16* output = (opus_int16*)outbuf;
+               samples_decoded = opus_decode(dec,
+                                             packet,
+                                             len,
+                                             &output[samp_count*num_channels],
+                                             frame_size,
+                                             0);
+               if (samples_decoded != frame_size) {
+                   fprintf(stderr, "opus_decode() returned %d\n", samples_decoded);
+                   ret = -1;
+                   break;
+               }
             }
          }
       }
@@ -484,18 +516,8 @@ void test_opus_custom(const int num_encoders, const int num_setting_changes) {
          params.float_encode = 0;
          params.float_decode = 0;
 #endif
-         if (params.custom_encode) {
-            params.encoder_bit_depth = RAND_SAMPLE(encoder_bit_depths);
-         }
-         else {
-            params.encoder_bit_depth = 16;
-         }
-         if (params.custom_decode) {
-            params.decoder_bit_depth = RAND_SAMPLE(decoder_bit_depths);
-         }
-         else {
-            params.decoder_bit_depth = 16;
-         }
+         params.encoder_bit_depth = RAND_SAMPLE(encoder_bit_depths);
+         params.decoder_bit_depth = RAND_SAMPLE(decoder_bit_depths);
 #ifdef RESYNTH
          /* Resynth logic works best when encoder/decoder use same datatype */
          params.float_decode = params.float_encode;
