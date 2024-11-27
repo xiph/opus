35e78ea43ec51345dc1cd9b66ebd66a4c0218a05
Jean-Marc Valin <jeanmarcv@google.com>
2024-11-27 16:50:09 -0500

    Try allocating at least 1 bit depth to high bands

--- celt/celt_encoder.c
+++ celt/celt_encoder.c
@@ -2529,7 +2529,7 @@ int celt_encode_with_ec(CELTEncoder * OPUS_RESTRICT st, const opus_res * pcm, in
 
    qext_bits = ((opus_int32)qext_bytes*8<<BITRES) - (opus_int32)ec_tell_frac(enc) - 1;
    clt_compute_extra_allocation(mode, qext_mode, start, end, qext_end, bandLogE, qext_bandLogE,
-         qext_bits, extra_pulses, extra_quant, C, LM, &ext_enc, 1);
+         qext_bits, extra_pulses, extra_quant, C, LM, &ext_enc, 1, tone_freq, toneishness);
    OPUS_COPY(error_bak, error, C*nbEBands);
    if (qext_bytes > 0) {
       quant_fine_energy(mode, start, end, oldBandE, error, fine_quant, extra_quant, &ext_enc, C);
