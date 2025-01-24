3058dcc9a0d6b73d7e081cdb6ab82b5b77936bb7
Jean-Marc Valin <jeanmarcv@google.com>
2025-01-24 16:31:41 -0500

    Support for QEXT decoding in Opus API

--- silk/define.h
+++ silk/define.h
@@ -64,7 +64,11 @@ extern "C"
 
 /* Maximum sampling frequency */
 #define MAX_FS_KHZ                              16
+#ifdef ENABLE_QEXT
+#define MAX_API_FS_KHZ                          96
+#else
 #define MAX_API_FS_KHZ                          48
+#endif
 
 /* Signal types */
 #define TYPE_NO_VOICE_ACTIVITY                  0
