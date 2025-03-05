c13f795e317a307013af04f7d22e20e11c45f76a
Jean-Marc Valin <jeanmarcv@google.com>
2025-03-05 10:37:47 -0500

    Update test scripts

--- tests/random_config.sh
+++ tests/random_config.sh
@@ -37,12 +37,13 @@ opt=`echo -e "-O1\n-O2\n-O3" | shuf -n1`
 
 arch=`echo -e "\n-march=core2\n-march=sandybridge\n-march=broadwell\n-march=skylake" | shuf -n1`
 
+resynth=`echo -e "\n-DRESYNTH" | shuf -n1`
 footprint=`echo -e "\n-DSMALL_FOOTPRINT" | shuf -n1`
 std=`echo -e "\n-std=c90\n-std=c99\n-std=c11\n-std=c17" | shuf -n1`
 sanitize=`echo -e "\n-fsanitize=address -fno-sanitize-recover=all\n-fsanitize=undefined -fno-sanitize-recover=all -fsanitize-recover=signed-integer-overflow -DDISABLE_PTR_CHECK" | shuf -n1`
 
 
-CFLAGS="$CFLAGS $std $opt $arch $footprint $math $sanitize"
+CFLAGS="$CFLAGS $resynth $std $opt $arch $footprint $math $sanitize"
 
 echo "CFLAGS=$CFLAGS" > "$config"
 
@@ -50,7 +51,8 @@ lib=`echo -e "\n--disable-static\n--disable-shared" | shuf -n1`
 
 arithmetic=`echo -e "\n--enable-deep-plc\n--enable-dred\n--enable-osce\n--enable-dred --enable-osce\n--enable-fixed-point\n--enable-fixed-point --enable-fixed-point-debug\n--enable-fixed-point --disable-float-api\n--enable-fixed-point --enable-fixed-point-debug --disable-float-api" | shuf -n1`
 
-custom=`echo -e "\n--enable-custom-modes" | shuf -n1`
+custom=`echo -e "\n--enable-custom-modes\n--enable-opus-custom-api" | shuf -n1`
+res=`echo -e "--enable-fixed-res24\n--enable-fixed-res24 --enable-qext\n" | shuf -n1`
 
 asm=`echo -e "\n--disable-asm\n--disable-rtcd\n--disable-intrinsics" | shuf -n1`
 #asm=`echo -e "\n--disable-asm\n--disable-intrinsics" | shuf -n1`
@@ -71,7 +73,7 @@ fi
 echo using testvectors at "$vectors" >> "$config"
 
 
-config_opt="$lib $arithmetic $custom $asm $assert $harden $fuzz $checkasm $rfc8251 $approx $lossgen"
+config_opt="$lib $arithmetic $custom $res $asm $assert $harden $fuzz $checkasm $rfc8251 $approx $lossgen"
 
 echo configure $config_opt >> "$config"
 
@@ -110,7 +112,7 @@ fi
 
 rate=`echo -e "8000\n12000\n16000\n24000\n48000" | shuf -n1`
 echo testvectors for "$rate" Hz > testvectors_output.txt
-../../../run_vectors.sh . "$vectors" "$rate" >> testvectors_output.txt 2>&1
+../../tests/run_vectors.sh . "$vectors" "$rate" >> testvectors_output.txt 2>&1
 
 if [ $? -ne 0 ]
 then
@@ -123,4 +125,5 @@ echo all tests PASS >> "$config"
 #When everything's good, do some cleaning up to save space
 make distclean > /dev/null 2>&1
 rm -f tmp.out
+rm -rf silk celt dnn src tests doc
 gzip make_output.txt
