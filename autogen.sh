02b2691ee150f04a487204c1ce9787b15dc0ac31
Jan Buethe <jan.buethe@gmx.net>
2025-03-30 17:25:47 -0700

    fixed broken autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -9,11 +9,7 @@ set -e
 srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
-<<<<<<< HEAD
 dnn/download_model.sh "66d438011afadd36428794dedf39e7c44843f03f708046f53ce605b8533629ca"
-=======
-dnn/download_model.sh "160753e983198f29f1aae67c54caa0e30bd90f1ce916a52f15bdad2df8e35e58"
->>>>>>> eb39efaa (Fixing lossgen to work with USE_WEIGHTS_FILE)
 
 echo "Updating build configuration files, please wait...."
 
