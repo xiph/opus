57a2042d359e9d83f9bf0eea421d133e9f709083
Mark Harris <mark.hsj@gmail.com>
2025-11-24 21:10:54 -0800

    dnn/download_model.sh: Try shasum if no sha256sum
    macOS comes with shasum rather than sha256sum.

--- dnn/download_model.sh
+++ dnn/download_model.sh
@@ -4,32 +4,37 @@ set -e
 model=opus_data-$1.tar.gz
 
 if [ ! -f $model ]; then
-        echo "Downloading latest model"
-        if command -v wget >/dev/null; then
-          wget -O $model https://media.xiph.org/opus/models/$model
-        else
-          # if wget is not available use curl
-          curl -o $model https://media.xiph.org/opus/models/$model
-        fi
+   echo "Downloading latest model"
+   if command -v wget >/dev/null; then
+      wget -O $model https://media.xiph.org/opus/models/$model
+   else
+      # if wget is not available use curl
+      curl -o $model https://media.xiph.org/opus/models/$model
+   fi
 fi
 
-if command -v sha256sum
-then
+if command -v sha256sum >/dev/null; then
+   SHA256SUM=sha256sum
+elif command -v shasum >/dev/null; then
+   SHA256SUM="shasum -a 256"
+else
+   SHA256SUM=
+fi
+
+if [ -n "$SHA256SUM" ]; then
    echo "Validating checksum"
    checksum="$1"
-   checksum2=$(sha256sum $model | awk '{print $1}')
+   checksum2=$($SHA256SUM $model | awk '{print $1}')
    if [ "$checksum" != "$checksum2" ]
    then
       echo "Aborting due to mismatching checksums. This could be caused by a corrupted download of $model."
       echo "Consider deleting local copy of $model and running this script again."
       exit 1
    else
-      echo "checksums match"
+      echo "Checksums match"
    fi
 else
-   echo "Could not find sha256 sum; skipping verification. Please verify manually that sha256 hash of ${model} matches ${1}."
+   echo "Could not find sha256sum or shasum; skipping verification. Please verify manually that sha256 hash of ${model} matches ${1}."
 fi
-
-
 
 tar xvzomf $model
